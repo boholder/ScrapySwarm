35f714117cb5ade31b4c54c271e12dd57f70712c
boholder <bottleholder@anche.no>
2019-06-28 08:05:17 +0800

    add log_util but not implement

--- ScrapySwarm/centercontrol/log_util.py
+++ ScrapySwarm/centercontrol/log_util.py
@@ -6,6 +6,13 @@
 
 @Author : Boholder
 
-@Function : 
+@Function : 两个类，spider_log_util被爬虫调用，
+            cc_log_util被cc-api模块调用，
+            向MongoDB的对应log集合中插入|更新内容。
 
+            想了想，python logging 记录的信息太少了，
+            只有一个等级一个信息，信息还整个是个str。
+            干脆直接写个封装，再插入数据库。
+
+            scrapy本身的log在cc-api中控制是否写入一个文本文件
 '''
